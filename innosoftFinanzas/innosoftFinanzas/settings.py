d050c58f3d361399ff98fd9698387231e34d12e5
practica <practica@pc-14-148.etsii>
2021-12-15 09:57:35 +0100

    Heroku

--- innosoftFinanzas/innosoftFinanzas/settings.py
+++ innosoftFinanzas/innosoftFinanzas/settings.py
@@ -135,3 +135,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+BASEURL = 'https://innosoft-finanzas'
+APIS = {}
+import django_heroku
+django_heroku.settings(locals())
