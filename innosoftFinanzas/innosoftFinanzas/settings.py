896a1cd053214ae3bd7ca27e5cd9ea1a04911adb
Gonzalo <62102082+gonlalle@users.noreply.github.com>
2021-12-21 22:35:46 +0100

    Update settings.py

--- innosoftFinanzas/innosoftFinanzas/settings.py
+++ innosoftFinanzas/innosoftFinanzas/settings.py
@@ -145,6 +145,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+NOSE_ARGS = [
+    '--with-xunit'
+]
 
 django_heroku.settings(locals(),test_runner=False)
