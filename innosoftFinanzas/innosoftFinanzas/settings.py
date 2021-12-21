523011c819bedcc73b2af76c91069f0cc3c69a2e
Gonzalo <62102082+gonlalle@users.noreply.github.com>
2021-12-21 22:37:21 +0100

    Update settings.py

--- innosoftFinanzas/innosoftFinanzas/settings.py
+++ innosoftFinanzas/innosoftFinanzas/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
