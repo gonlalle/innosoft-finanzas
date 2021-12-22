ef879c01d27670ef683bfd1d79301a068ad7fd13
Gonzalo <62102082+gonlalle@users.noreply.github.com>
2021-12-22 17:34:17 +0100

    Update wsgi.py

--- innosoftFinanzas/innosoftFinanzas/wsgi.py
+++ innosoftFinanzas/innosoftFinanzas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innosoftFinanzas.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "<PROJ_NAME>.settings"
 
 application = get_wsgi_application()
