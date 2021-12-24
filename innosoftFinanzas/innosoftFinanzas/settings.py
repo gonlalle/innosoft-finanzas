1db413b2729a8310c4a21bc5b6b1a1aa8196d05d
Geniusbat <moligus@gmail.com>
2021-12-24 19:08:34 +0100

    database updated and requirements set properly

--- innosoftFinanzas/innosoftFinanzas/settings.py
+++ innosoftFinanzas/innosoftFinanzas/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'innosoftFinanzas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'finanzas',
-        'USER': 'postgres',
-        'PASSWORD': 'asdf1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
